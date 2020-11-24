0a2e9ef26d1ebf36e15753ea29d86430860a6179
Robert <71959725+prajs1@users.noreply.github.com>
2020-11-24 23:34:00 +0100

    a

--- admin/functions/menu.php
+++ admin/functions/menu.php
@@ -3,19 +3,7 @@
   {
     $menu = "<div id="menu"><ol id=\"ol\">";
 
-    $query_menu = "SELECT * FROM menu";
-
-    $wynik_menu = $mysqli->query($query_menu) or die("Zapytanie query_menu nie działa");
-
-    if ($wynik_menu->num_rows)
-    {
-      while ($a = $wynik_menu->fetch_assoc())
-      {
-        if($a['active'] == 'y'){
-          $menu .= "<li><a href=\"".$a['link']."\">".$a['name']."</a></li>";
-        }
-      }
-    }
+    
 
     $menu .="</ol></div>";
 

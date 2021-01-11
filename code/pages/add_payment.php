4d540de6ab27d5f37e2cb254614850e7888b3e41
Robert <71959725+prajs1@users.noreply.github.com>
2021-01-11 16:27:42 +0100

    add payment page

--- code/pages/add_payment.php
+++ code/pages/add_payment.php
@@ -1,1 +1,17 @@
 <!-- TODO postrona z dodawaniem płatności -->
+<form method="POST">
+  <fieldset class="fieldsets"><legend><b><i>Dodaj płatność</i></b></legend>
+    <select name="add_payment_recipents_select" id="add_payment_recipents_select">
+      <?php
+        $recipents = getRecipents($mysqli);
+
+        foreach ($recipents as $recipent) {
+          echo "<option value=\"recipent" . $recipent['id_recipent'] . "\">" . $recipent['name'] . "</option>";
+        }
+      ?>
+    </select><br>
+    <input type="checkbox" name="add_payment_recipents_checkbox" id="add_payment_recipents_checkbox"> Przelew stały <br>
+    Kwota przelewu: <input type="number" name="add_payment_recipents_amount" id="add_payment_recipents_amount" min="0">
+    <input type="button" value="Prześlij" class="button" onclick="">
+  </fieldset>
+</form>
